d1824b71e37c0d8867a604a67e17864c017560db
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-15 01:43:00 -0400

    commit tree 682dcf6debc6d15189626e22ae474774bf32f05a

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!"
+  } else if (distance > 400 && distance <= 2000 ) {
+    return "I will gladly take your thirty bucks."
+  } else if (distance > 2000 && distance <= 2500) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;
+    default:
+      response = "Bye."
+      break;   
+  }
+  return response;
 }
